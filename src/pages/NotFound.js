053c1a8fb05d1e9bde9dbb4ff7cfc319181b8e96
BaltoClem <dumasclementjulien@yahoo.fr>
2022-06-05 14:53:14 +0200

    Mise en page du NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            Ici c'est Notfound
+        <div className="notFound">
+            <div className="notFoundContent">
+                <h3>Désolé, cette page n'existe pas...</h3>
+                <NavLink exat to="/">
+                <lord-icon
+                    src="https://cdn.lordicon.com/qhaguvvs.json"
+                    trigger="loop">
+                </lord-icon>
+                <span> Retour à l'accueil</span>
+                </NavLink>
+            </div>
         </div>
     );
 };
