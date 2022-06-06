9970f7e839a80d4c446158f3ca6a1931d2e1d162
BaltoClem <dumasclementjulien@yahoo.fr>
2022-06-06 23:21:43 +0200

    Mise en forme du component Languages

--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ProgressBar from './ProgressBar';
 
-const Languages = () => {
-    return (
-        <div>
-            Languages et Frameworks
-        </div>
-    );
-};
+class Languages extends Component {
+    state = {
+        languages: [
+            {id: 1, value:"HTML 5", xp:1},
+            {id: 2, value:"CSS 3", xp:1},
+            {id: 3, value:"JavaScript", xp:1},
+            {id: 4, value:"PHP", xp:1},
+        ],
+        frameworks: [
+            {id: 1, value:"Bootstrap", xp:1},
+            {id: 2, value:"CodeIgniter", xp:1},
+            {id: 1, value:"Symfony 5", xp:1},
+            {id: 1, value:"React", xp:0.2},
+        ]
+    }
+    render() {
+        let {languages, frameworks} = this.state;
+        return (
+            <div className="languagesFrameworks">
+                <ProgressBar 
+                    languages={languages}
+                    className="languagesDisplay"
+                    title="languages"
+                />
+                <ProgressBar
+                    languages={frameworks}
+                    title="frameworks & bibliothèques"
+                    className="frameworksDisplay"
+                />
+            </div>
+        );
+    }
+}
 
 export default Languages;
