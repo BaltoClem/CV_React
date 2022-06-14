b86fd065509dbf4e47550d84940b76e033996a7e
BaltoClem <dumasclementjulien@yahoo.fr>
2022-06-15 01:33:55 +0200

    Mise en page Experience

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Experience = () => {
     return (
         <div>
-            Experience
+            <div className="experience">
+                <h3>Experience</h3>
+                <div className="exp-1">
+                    <h4>Directeur Technique - Apple</h4>
+                    <h5>2018-2020</h5>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, 
+                        nisi inventore praesentium natus tempora dicta nam aspernatur, laboriosam illo autem saepe qui 
+                        asperiores debitis nihil amet molestias quia tenetur. Necessitatibus repellendus nostrum blanditiis, 
+                        culpa assumenda maxime hic vero!</p>
+                </div>
+                <div className="exp-2">
+                    <h4>Directeur Technique - Apple</h4>
+                    <h5>2018-2020</h5>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, 
+                        nisi inventore praesentium natus tempora dicta nam aspernatur, laboriosam illo autem saepe qui 
+                        asperiores debitis nihil amet molestias quia tenetur. Necessitatibus repellendus nostrum blanditiis, 
+                        culpa assumenda maxime hic vero!</p>
+                </div>
+            </div>
         </div>
     );
 };
